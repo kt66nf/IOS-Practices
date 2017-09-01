f7826de19466fcbc13bc3126002b4a667e781ae8
Kim, Youngwook <youngwoo@186590d99257.local>
2017-08-31 18:25:24 -0700

    Initial Commit

---
+++ udemy practiceTests/udemy_practiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  udemy_practiceTests.swift
+//  udemy practiceTests
+//
+//  Created by Kim, Youngwook on 8/31/17.
+//  Copyright © 2017 Kim, Youngwook. All rights reserved.
+//
+
+import XCTest
+@testable import udemy_practice
+
+class udemy_practiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
